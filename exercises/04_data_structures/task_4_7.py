4cdbd71d16f8a8554cb0f350ea5cbb20d1fc167d
easypythoncode <58902135+easypythoncode@users.noreply.github.com>
2022-08-03 09:18:19 +0300

    Добавлено решение задачи 4_7

--- exercises/04_data_structures/task_4_7.py
+++ exercises/04_data_structures/task_4_7.py
@@ -16,3 +16,10 @@
 """
 
 mac = "AAAA:BBBB:CCCC"
+# answer
+mac = mac.split(':')
+a = bin(int(mac[0], 16))
+b = bin(int(mac[1], 16))
+c = bin(int(mac[2], 16))
+out = a + b + c
+print(out.lstrip('0b'))
