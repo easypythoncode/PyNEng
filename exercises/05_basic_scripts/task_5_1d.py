87463e5ec37c2d5adac61f2957154fe687d0f685
easypythoncode <58902135+easypythoncode@users.noreply.github.com>
2022-08-05 09:06:07 +0300

    Добавлено дополнительное решение задачи 5_1d

--- exercises/05_basic_scripts/task_5_1d.py
+++ exercises/05_basic_scripts/task_5_1d.py
@@ -43,3 +43,9 @@ london_co = {
         "routing": True,
     },
 }
+
+# answer
+device = input('Введите имя устройства: ')
+keys = ', '.join(london_co[device].keys())
+parametr = input('Введите имя параметра ({}): '.format(keys))
+print(london_co[device].get((parametr.lower()), 'Такого параметра нет'))
