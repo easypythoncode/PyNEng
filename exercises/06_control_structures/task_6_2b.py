02119b66475832da455a106dfa4cfdef56819634
easypythoncode <58902135+easypythoncode@users.noreply.github.com>
2022-08-10 17:23:17 +0300

    Добавлено решение задачи 6_2b

--- exercises/06_control_structures/task_6_2b.py
+++ exercises/06_control_structures/task_6_2b.py
@@ -12,3 +12,27 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+
+# answwer
+chek = True
+ip = input('Введите IP адрес: ')
+octet = ip.split('.')
+while chek == True:
+    for a in octet:
+        if len(octet) != 4 or not a.isdigit() or int(a) < 0 or int(a) > 255:
+            print('Неправильный IP-адрес')
+            ip = input('Введите IP адрес: ')
+            octet = ip.split('.')
+            break
+        else:
+            chek = False
+if int(octet[0]) >= 1 and int(octet[0]) <= 223:
+    print('unicast')
+elif int(octet[0]) >= 224 and int(octet[0]) <= 239:
+    print('multicast')
+elif '255.255.255.255' in ip:
+    print('local broadcast')
+elif '0.0.0.0' in ip:
+    print('unassigned')
+else:
+    print('unused')
