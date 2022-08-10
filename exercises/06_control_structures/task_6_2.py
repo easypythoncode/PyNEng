78e6b715e1c90d4ef19f66110c8d7ddecd7b74fa
easypythoncode <58902135+easypythoncode@users.noreply.github.com>
2022-08-10 09:39:48 +0300

    Добавлено решение задачи 6_2

--- exercises/06_control_structures/task_6_2.py
+++ exercises/06_control_structures/task_6_2.py
@@ -12,3 +12,17 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+
+# answwer
+ip = input('Введите IP адрес: ')
+octet = ip.split('.')
+if int(octet[0]) >= 1 and int(octet[0]) <= 223:
+    print('unicast')
+elif int(octet[0]) >= 224 and int(octet[0]) <= 239:
+    print('multicast')
+elif '255.255.255.255' in ip:
+    print('local broadcast')
+elif '0.0.0.0' in ip:
+    print('unassigned')
+else:
+    print('unused')
