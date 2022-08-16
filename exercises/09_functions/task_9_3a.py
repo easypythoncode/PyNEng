caf8b779fe7356c5031fe80391ec43c3fe227be6
easypythoncode <58902135+easypythoncode@users.noreply.github.com>
2022-08-16 18:21:18 +0300

    Добавлено решение задачи 9_3a

--- exercises/09_functions/task_9_3a.py
+++ exercises/09_functions/task_9_3a.py
@@ -25,3 +25,34 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+
+
+# answer
+def get_int_vlan_map(config_filename):
+    access_dict = {}
+    trunk_dict = {}
+    result = ()
+    with open(config_filename) as f:
+        for line in f:
+            allowed = []
+            line = line.rstrip()
+            if '/' in line:
+                intf = (line.split())[-1]
+            elif 'switchport access vlan' in line and line[-1].isdigit():
+                access_dict[intf] = int(line.split()[-1])
+            elif 'switchport mode access' in line and line[-1].isalpha():
+                access_dict[intf] = 1
+            elif 'switchport trunk allowed vlan' in line:
+                vlan = line.split()[-1].split(',')
+                for element in vlan:
+                    allowed.append(int(element))
+                trunk_dict[intf] = allowed
+    result = list(result)
+    result.append(access_dict)
+    result.append(trunk_dict)
+    result = tuple(result)
+    return result
+
+
+a = get_int_vlan_map('config_sw2.txt')
+print(a)
