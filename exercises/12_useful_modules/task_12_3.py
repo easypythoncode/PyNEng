d69713953fa2e121c56ecff1ad36f04543be717e
easypythoncode <dzianis.zhukau@gmail.com>
2022-08-22 21:38:42 +0300

    Добавлено решение задачи 12_3

--- exercises/12_useful_modules/task_12_3.py
+++ exercises/12_useful_modules/task_12_3.py
@@ -18,3 +18,25 @@ Reachable    Unreachable
              10.1.1.9
 
 """
+
+# answer
+
+from task_12_1 import ping_ip_addresses
+from task_12_2 import convert_ranges_to_ip_list
+from tabulate import tabulate
+
+
+def print_ip_table(value1, value2):
+    dict = {}
+    dict['Reachable'] = value1
+    dict['Unreachable'] = value2
+    result = (tabulate(dict, headers='keys'))
+    return result
+
+
+if __name__ == "__main__":
+    result1 = convert_ranges_to_ip_list(['8.8.4.4', '1.1.1.1-3', '172.21.41.128-172.21.41.132'])
+    # result1 = convert_ranges_to_ip_list(['8.8.4.4', '192.168.1.1'])
+    resalt2 = ping_ip_addresses(result1)
+    result3 = print_ip_table(list(resalt2[0]), list(resalt2[1]))
+    print(result3)
