0f6c3e9195b27b876ac805b536a3411fc8f56a49
easypythoncode <dzianis.zhukau@gmail.com>
2022-08-29 12:53:22 +0300

    Добавлено решение задачи 17_3

--- exercises/17_serialization/task_17_3.py
+++ exercises/17_serialization/task_17_3.py
@@ -24,3 +24,32 @@ R6           Fa 0/2          143           R S I           2811       Fa 0/0
 
 Проверить работу функции на содержимом файла sh_cdp_n_sw1.txt
 """
+
+# answer
+
+import re
+from pprint import pprint
+
+
+def parse_sh_cdp_neighbors(value):
+    list = value.split('\n')
+    dict = {}
+    regex1 = r'(?P<key>\S+)>'
+    regex2 = r'(?P<rdev>\S+\d+) +(?P<lintf>\S+ \d+/\d+).+  (?P<rintf>\S+ \d+/\d+)'
+    for line in list:
+        match1 = re.search(regex1, line)
+        match2 = re.search(regex2, line)
+        if match1:
+            ldev = match1.group(1)
+            dict[ldev] = {}
+            continue
+        elif match2:
+            rdev, lintf, rintf = match2.group("rdev", "lintf", "rintf")
+            dict[ldev][lintf] = {rdev: rintf}
+    return dict
+
+
+if __name__ == "__main__":
+    with open("sh_cdp_n_sw1.txt") as f:
+        call = parse_sh_cdp_neighbors(f.read())
+    pprint(call)
